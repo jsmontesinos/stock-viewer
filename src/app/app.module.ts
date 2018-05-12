6d78a221540f7743fb0709bccef48dee0e4b65d4
Juan Sánchez <jsmontesinos@gmail.com>
2018-05-12 14:18:16 +0200

    Add Angular Manterial and a cupple of UI components. All working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule, MatSelectModule, MatFormFieldModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+const MaterialModules = [MatButtonModule, MatSelectModule, MatFormFieldModule];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ... MaterialModules,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
